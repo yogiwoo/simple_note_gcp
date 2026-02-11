45f6ed8b994ced162722eba40ecbf182fe7fa9c0
yogesh kumar <yogiwoo@gmail.com>
2026-02-11 21:11:02 +0530

    mongodb error

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 4005;
 const notesRouter = require('./routes/notes');
 const userRouter=require('./routes/auth')
 const authorize=require('./middleware/auth')
 // MongoDB Connection
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/gcp';
+const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI, {
 }).then(() => {
-  console.log('MongoDB connected successfully');
+  console.log('MongoDB connected successfully',MONGO_URI);
 }).catch(err => {
   console.error('MongoDB connection error:', err);
 });
